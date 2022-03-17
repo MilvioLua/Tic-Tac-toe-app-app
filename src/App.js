fa58ed2ec1b8618971189121cb5a29d03da7bdcd
Milvio LuA <milviolua@gmail.com>
2022-03-17 11:44:01 +0700

    remove bug

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React tes
+          Learn React 
         </a>
       </header>
     </div>
@@ -23,3 +23,5 @@ function App() {
 }
 
 export default App;
+
+
